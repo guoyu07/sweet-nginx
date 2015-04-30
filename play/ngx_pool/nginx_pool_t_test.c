a8ff12cdba34d70e2056a90d5eede6defeb5d443
rryqszq4 <279664262@163.com>
2015-04-30 19:42:07 +0800

    [play] update ngx pool test

--- play/ngx_pool/nginx_pool_t_test.c
+++ play/ngx_pool/nginx_pool_t_test.c
@@ -16,5 +16,59 @@ void ngx_log_error_core(ngx_utin_t level, ngx_log_t *log, ngx_err_t err,
 
 void dump_pool(ngx_pool_t* pool)
 {
-	
+	while (pool){
+		printf("pool = 0x%x\n",pool);
+		printf("	.d\n");
+		printf("		.last = 0x%x\n", pool->d.last);
+		printf("		.end  = 0x%x\n", pool->d.end);
+		printf("		.next = 0x%x\n", pool->d.next);
+		printf("		.failed = 0x%x\n", pool->d.failed);
+		printf("	.max = %d\n", pool->max);
+		printf("	.current = 0x%x\n", pool->current);
+		printf("	.chain = 0x%x\n", pool->chain);
+		printf("	.large = 0x%x\n", pool->large);
+		printf("	.cheanup = 0x%x\n", pool->cleanup);
+		printf("	.log = 0x%x\n", pool->log);
+		printf("available pool memory = %d\n\n", pool->d.end - pool->d.last);
+		pool = pool->d.next;
+	}
 }
+
+int main()
+{
+	ngx_pool_t *pool;
+
+	printf("--------------------\n");
+	printf("create a new pool:\n");
+	printf("--------------------\n");
+	pool = ngx_create_pool(1024, NULL);
+	dump_pool(pool);
+
+	printf("--------------------\n");
+	printf("alloc block 1 from the pool:\n");
+	printf("--------------------\n");
+	ngx_palloc(pool, 512);
+	dump_pool(pool);
+
+	printf("--------------------\n");
+	printf("alloc block 2 from the pool:\n");
+	printf("--------------------\n");
+	ngx_palloc(pool, 512);
+	dump_pool(pool);
+
+	printf("--------------------\n");
+	printf("alloc block 3 from the pool:\n");
+	printf("--------------------\n");
+	ngx_palloc(pool, 512);
+	dump_pool(pool);
+
+	ngx_destroy_pool(pool);
+
+	return 0;
+}
+
+
+
+
+
+
