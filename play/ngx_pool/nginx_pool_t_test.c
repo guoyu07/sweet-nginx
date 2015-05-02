c427b4e108dd14ce001b8154ba6dda0d8d2ca07a
rryqszq4 <279664262@163.com>
2015-05-02 23:21:02 +0800

    [play] compile nginx_pool_t_test

--- play/ngx_pool/nginx_pool_t_test.c
+++ play/ngx_pool/nginx_pool_t_test.c
@@ -8,8 +8,8 @@
 
 volatile ngx_cycle_t	*ngx_cycle;
 
-void ngx_log_error_core(ngx_utin_t level, ngx_log_t *log, ngx_err_t err,
-	const char *fmt, ...)
+void ngx_log_error_core(ngx_uint_t level, ngx_log_t *log, ngx_err_t err,
+    const char *fmt, ...)
 {
 
 }
@@ -17,18 +17,18 @@ void ngx_log_error_core(ngx_utin_t level, ngx_log_t *log, ngx_err_t err,
 void dump_pool(ngx_pool_t* pool)
 {
 	while (pool){
-		printf("pool = 0x%x\n",pool);
+		printf("pool = 0x%p\n",pool);
 		printf("	.d\n");
-		printf("		.last = 0x%x\n", pool->d.last);
-		printf("		.end  = 0x%x\n", pool->d.end);
-		printf("		.next = 0x%x\n", pool->d.next);
-		printf("		.failed = 0x%x\n", pool->d.failed);
+		printf("		.last = 0x%p\n", pool->d.last);
+		printf("		.end  = 0x%p\n", pool->d.end);
+		printf("		.next = 0x%p\n", pool->d.next);
+		printf("		.failed = 0x%d\n", pool->d.failed);
 		printf("	.max = %d\n", pool->max);
-		printf("	.current = 0x%x\n", pool->current);
-		printf("	.chain = 0x%x\n", pool->chain);
-		printf("	.large = 0x%x\n", pool->large);
-		printf("	.cheanup = 0x%x\n", pool->cleanup);
-		printf("	.log = 0x%x\n", pool->log);
+		printf("	.current = 0x%p\n", pool->current);
+		printf("	.chain = 0x%p\n", pool->chain);
+		printf("	.large = 0x%p\n", pool->large);
+		printf("	.cheanup = 0x%p\n", pool->cleanup);
+		printf("	.log = 0x%p\n", pool->log);
 		printf("available pool memory = %d\n\n", pool->d.end - pool->d.last);
 		pool = pool->d.next;
 	}
